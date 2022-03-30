56c853db24f99eb38ab0bc7d81b9f209871e0b0a
Daniel Fich <danielfich2@gmail.com>
2022-03-30 10:49:10 +0200

    Android works + touch in StaticDraggable

--- DDP UI Project/Assets/StaticDraggable.cs
+++ DDP UI Project/Assets/StaticDraggable.cs
@@ -22,4 +22,15 @@ public class StaticDraggable : MonoBehaviour
     {
         transform.position = (Vector2)cam.ScreenToWorldPoint(Input.mousePosition) - difference;
     }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = cam.ScreenToWorldPoint(touch.position);
+            transform.position = touchPosition;
+        }
+    }
+
 }
