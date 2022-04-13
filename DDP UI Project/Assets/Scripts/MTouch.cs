4dfbb5366ff0380ce3bca48209945954d5768596
Daniel Fich <danielfich2@gmail.com>
2022-04-13 10:46:50 +0200

    Moved a lot of .cs-files
    * Files moved from Scripts to Scripts/MonoBehaviour
    * MTouch and MTouchDict moved to new MTouch.cs-file

---
+++ DDP UI Project/Assets/Scripts/MTouch.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MTouch
+{
+    public Vector2 startPos;
+    public Vector2 pos;
+
+    public MTouch(Vector2 _startPos, Vector2 _pos)
+    {
+        startPos = _startPos;
+        pos = _pos;
+    }
+
+    public MTouch(Vector2 _samePos)
+    {
+        startPos = pos = _samePos;
+    }
+}
+
+// Currently not in use. Might be useful eventually.
+public class MTouchDict : IEnumerable
+{
+    public Dictionary<int, MTouch> mTouches;
+
+    public MTouchDict()
+    {
+        mTouches = new Dictionary<int, MTouch>();
+    }
+
+    public void Add(Vector2 _samePos, int fingerId)
+    {
+        mTouches.Add(fingerId, new MTouch(_samePos));
+    }
+
+    // Currently not in use
+    public void Add(Vector2 _startPos, Vector2 _pos, int fingerId)
+    {
+        mTouches.Add(fingerId, new MTouch(_startPos, _pos));
+    }
+
+    public void SetPos(Vector2 _pos, int fingerId)
+    {
+        if (mTouches.ContainsKey(fingerId))
+            mTouches[fingerId].pos = _pos;
+    }
+
+    public void Remove(int fingerId)
+    {
+        mTouches.Remove(fingerId);
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        return mTouches.GetEnumerator();
+    }
+}
