c5a285be8fefb282b73d911ecc5155ab7914a564
Daniel Fich <danielfich2@gmail.com>
2022-05-15 12:58:37 +0200

    More queue stuff...

--- DDP UI Project/Assets/Scripts/MonoBehaviour/DiscQueueParent.cs
+++ DDP UI Project/Assets/Scripts/MonoBehaviour/DiscQueueParent.cs
@@ -9,16 +9,27 @@ public class DiscQueueParent : MonoBehaviour
 
     [Header("Settings")]
     public float passDiscDelay;
+    public int startHideIndex = -1;
+
+    private DiscQueue[] children;
 
     private void Awake()
     {
         SetChildrenReferences();
+
+        //HideFrom(StartHideIndex);
+    }
+
+    private void Update()
+    {
+        //HideFrom(startHideIndex);
+        Stuff();
     }
 
     void SetChildrenReferences()
     {
-        DiscQueue[] children = GetComponentsInChildren<DiscQueue>();
-
+        children = GetComponentsInChildren<DiscQueue>();
+    
         SnapArea next = discPlayer;
 
         foreach (DiscQueue discQueue in children)
@@ -26,5 +37,48 @@ public class DiscQueueParent : MonoBehaviour
             discQueue.nextQueue = next;
             next = discQueue;
         }
+    }
+
+    void HideFrom(int index)
+    {
+        for (int i = 0; i < children.Length; i++)
+        {
+            bool currentActive = i < index || startHideIndex == -1;
+
+            children[i].gameObject.SetActive(currentActive);
+        }
+    }
+
+    void Stuff()
+    {
+        bool filled = true;
+
+        int hideFromIndex = -1;
+
+        int i = 0;
+        foreach (DiscQueue discQueue in children)
+        {
+            //*/
+            bool currentActive = i < startHideIndex || filled || discQueue.currentDisc != null || startHideIndex == -1;
+
+            //children[i].gameObject.SetActive(currentActive);
+
+            if (currentActive)
+                hideFromIndex = i + 1;
+
+            /*/
+
+            if (i < startHideIndex || filled || discQueue.currentDisc != null)
+                hideFromIndex = i;
+
+            //*/
+
+            if (discQueue.currentDisc == null)
+                filled = false;
+
+            i++;
+        }
+
+        HideFrom(hideFromIndex);
     }
 }
